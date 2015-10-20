1f779d6880a917bef9d5966c62ac7975b1306698
OasisTechnologies <electionguru4u@gmail.com>
2015-10-20 12:41:02 +0530

    added po db

--- app/src/main/java/oasis/com/smartchemist/databases/DatabasePurchaseOrder.java
+++ app/src/main/java/oasis/com/smartchemist/databases/DatabasePurchaseOrder.java
@@ -7,15 +7,54 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class DatabasePurchaseOrder extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "master_purchase";
+    private static final String DATABASE_NAME = "purchase_order";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String DATABASE_MASTER_PURCHASE_ORDER = "purchase_order";
+    private static final String DATABASE_MASTER_PURCHASE_ORDER = "master_purchase_order";
     private static final String KEY_ID = "_ID";
     private static final String PO_CODE = "po_code";
+    private static final String MPO_PO_DATE = "po_date";
+    private static final String MPO_PO_TIME= "po_time";
+    private static final String MPO_WHOLESALER_CODE= "wholesaler_code";
+    private static final String MPO_NO_OF_ITEMS= "no_of_items";
+    private static final String MPO_TOTAL_AMOUNT_WITHOUT_TAX= "total_amount_without_tax";
+    private static final String MPO_DISC_PER= "disc_per";
+    private static final String MPO_DISC_AMT= "disc_amt";
+    private static final String MPO_TAX= "tax";
+    private static final String MPO_CREDIT_NOTE= "credit_note";
+    private static final String MPO_GOODS_RETURN_AMOUNT= "goods_return_amount";
+    private static final String MPO_EXPIRED_GOODS_AMOUNT= "expired_goods_amount";
+    private static final String MPO_CHEQUE_DD_DISCOUNT= "cheque_dd_discount";
+    private static final String MPO_CASH_DISCOUNT= "cash_discount";
+    private static final String MPO_OTHER_AMOUNT= "other_amount";
+    private static final String MPO_TOTAL_AMOUNT_WITH_TAX= "total_amount_with_tax";
+    private static final String MPO_ROUNDING_FACTORS= "rounding_factors";
+    private static final String MPO_TOTAL_AMOUNT_WITH_ROUNDING_FACTORS= "total_amount_with_rounding_factors";
+    private static final String MPO_DUE_DATE= "due_date";
+    private static final String MPO_FINANCIAL_YEAR= "financial_year";
 
+    private static final String DATABASE_MASTER_PO_TAX = "master_po_tax";
+    //private static final String PO_CODE = "po_code";
+    private static final String TAX_CODE= "tax_code";
+    private static final String TOTAL_AMOUNT= "total_amount";
+    private static final String TAX_AMOUNT= "tax_amount";
 
     private static final String DATABASE_DETAIL_PURCHASE_ORDER = "detail_purchase_order";
+    //private static final String PO_CODE = "po_code";
+    private static final String DPO_BATCH_CODE = "batch_code";
+    private static final String DPO_QTY = "qty";
+    private static final String DPO_FREE_QTY = "free_qty";
+    private static final String DPO_RATE = "rate";
+//    private static final String MPT_TAX_CODE= "tax_code";
+    private static final String DPO_TOTAL_TAX= "total_tax";
+    private static final String DPO_TOTAL_AMOUNT_WITH_TAX= "total_amount_with_tax";
+
+    private static final String DATABASE_DETAIL_PO_TAX = "detail_po_tax";
+    //po_code,tax_code,total_amount,tax_amount;
+    //private static final String PO_CODE = "po_code";
+    //private static final String MPT_TAX_CODE= "tax_code";
+    //private static final String MPT_TOTAL_AMOUNT= "total_amount";
+    //private static final String MPT_TAX_AMOUNT= "tax_amount";
 
     public DatabasePurchaseOrder(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
--- app/src/main/java/oasis/com/smartchemist/utils/DetailPO.java
+++ app/src/main/java/oasis/com/smartchemist/utils/DetailPO.java
@@ -2,5 +2,48 @@ package oasis.com.smartchemist.utils;
 
 public class DetailPO {
 
+    private String po_code,batch_code,qty,free_qty,rate,tax_code,total_tax,total_amount_with_tax;
 
+    public DetailPO(String po_code, String batch_code, String qty, String free_qty, String rate, String tax_code, String total_tax, String total_amount_with_tax) {
+        this.po_code = po_code;
+        this.batch_code = batch_code;
+        this.qty = qty;
+        this.free_qty = free_qty;
+        this.rate = rate;
+        this.tax_code = tax_code;
+        this.total_tax = total_tax;
+        this.total_amount_with_tax = total_amount_with_tax;
+    }
+
+    public String getPo_code() {
+        return po_code;
+    }
+
+    public String getBatch_code() {
+        return batch_code;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public String getFree_qty() {
+        return free_qty;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public String getTax_code() {
+        return tax_code;
+    }
+
+    public String getTotal_tax() {
+        return total_tax;
+    }
+
+    public String getTotal_amount_with_tax() {
+        return total_amount_with_tax;
+    }
 }
